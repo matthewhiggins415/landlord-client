9a2a9abfc4ca4fa74c00b25a00dca35ae514e5e7
matthewhiggins415 <mhiggins.louis@gmail.com>
2021-11-29 13:44:43 -0700

    created all the views except for TenantDetails screen

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ let signupScreen = document.getElementById("signup")
 let loginScreen = document.getElementById("loginScreen")
 let propertyFormScreen = document.getElementById("propertyFormScreen")
 let propertyForm = document.getElementById("propertyForm")
-let tenantForm = document.getElementById("tenantForm")
+let tenantFormScreen = document.getElementById("tenantFormScreen")
 let tenantScreen = document.getElementById("tenantScreen")
 let tenantInfoScreen = document.getElementById("tenantInfoScreen")
 
 signupScreen.style.display = "none"
 loginScreen.style.display = "none"
 propertyScreen.style.display = "none"
+propertyFormScreen.style.display = "none"
+tenantScreen.style.display = "none"
+tenantFormScreen.style.display = "none"
