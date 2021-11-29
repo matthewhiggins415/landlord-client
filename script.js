be9673576f3eaab0cc2a5bbfbfa3b0405792578f
matthewhiggins415 <mhiggins.louis@gmail.com>
2021-11-29 12:51:12 -0700

    add views to html, added style.css, and script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let signupScreen = document.getElementById("signup")
+let loginScreen = document.getElementById("loginScreen")
+let propertyFormScreen = document.getElementById("propertyFormScreen")
+let propertyForm = document.getElementById("propertyForm")
+let tenantForm = document.getElementById("tenantForm")
+let tenantScreen = document.getElementById("tenantScreen")
+let tenantInfoScreen = document.getElementById("tenantInfoScreen")
+
+signupScreen.style.display = "none"
+loginScreen.style.display = "none"
+propertyScreen.style.display = "none"
