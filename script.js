b3ceb6967ec98f7301e0b877adc9e920e1ca2163
matthewhiggins415 <mhiggins.louis@gmail.com>
2021-11-30 10:45:13 -0700

    complete views, starting frontend api calls.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let getFormFields = require("./lib/get-form-fields")
+
+
 let signupScreen = document.getElementById("signup")
 let loginScreen = document.getElementById("loginScreen")
 let propertyFormScreen = document.getElementById("propertyFormScreen")
@@ -6,9 +9,23 @@ let tenantFormScreen = document.getElementById("tenantFormScreen")
 let tenantScreen = document.getElementById("tenantScreen")
 let tenantInfoScreen = document.getElementById("tenantInfoScreen")
 
-signupScreen.style.display = "none"
+
+// signupScreen.style.display = "none"
 loginScreen.style.display = "none"
 propertyScreen.style.display = "none"
 propertyFormScreen.style.display = "none"
 tenantScreen.style.display = "none"
 tenantFormScreen.style.display = "none"
+tenantInfoScreen.style.display = "none"
+
+//Elements of registration view
+let signUpForm = document.getElementById("signupForm")
+let signUpBtn = document.getElementById("signUpBtn")
+
+const submitRegistration = (e) => {
+  e.preventDefault()
+  console.log('click working')
+  // console.log(getFormFields(signUpForm))
+}
+
+signUpBtn.addEventListener('click', submitRegistration())
