56760ee0f5f30bf0534e9e5e8b86b8fddc6b20e2
matthewhiggins415 <mhiggins.louis@gmail.com>
2021-12-01 11:38:37 -0700

    ui for rgstr works, need rgstr success msg on login page

--- auth/ui.js
+++ auth/ui.js
@@ -1,14 +1,26 @@
 //This file contains logic for ui functionality
 const store = require('../app/store')
 
+let signUpScreenMessage = $("#signupScreenMessage")
+
+const showAMessage = (message) => {
+  signUpScreenMessage.text(`${message}`).css("display", "block")
+  setTimeout(function () {
+    signUpScreenMessage.empty().css("display", "none")
+  }, 3000)
+}
+
 //signUpSuccess
 const signUpSuccess = (responseData) => {
   console.log(responseData)
+  $("#signupScreen").fadeOut()
+  $("#loginScreen").fadeIn()
 }
 
 //signUpFailure
 const signUpFailure = (responseData) => {
   console.log(responseData)
+  showAMessage("Sign up failure!")
 }
 
 
