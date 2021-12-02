7fe9b28f5e9c8c49070ae703c296467a78e5654d
matthewhiggins415 <mhiggins.louis@gmail.com>
2021-12-02 12:47:24 -0700

    properties render dynamically on propScreen :), needs formatting.

--- property/ui.js
+++ property/ui.js
@@ -1,3 +1,4 @@
+const { properties } = require('../app/store')
 const store = require('../app/store')
 
 //Create property functionality
@@ -12,7 +13,27 @@ const createPropFailure = (responseData) => {
 //Get Users Properties functionality
 const getPropertiesSuccess = (responseData) => {
   store.properties = responseData.properties
-  console.log(store.properties)
+  let properties = store.properties
+  console.log(properties)
+  console.log(properties.length)
+
+  //Grab the parent
+  //Loop through the store.properties array
+  //create a child for parent element on every loop
+
+  let propertiesList = document.getElementById("propertiesList")
+
+
+  properties.forEach(property => {
+    for (let key in property) {
+      if (key === 'address') {
+        let p = document.createElement("p")
+        let address = property[key]
+        propertiesList.append(`${address}`, p)
+      }
+    }
+  })
+
 }
 
 const getPropertiesFailure = (responseData) => {
