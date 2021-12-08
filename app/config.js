c1cf5069f6c80a59f8f82ca1b0fc37bd8f009f9e
matthewhiggins415 <mhiggins.louis@gmail.com>
2021-12-07 19:01:04 -1000

    production url added to config

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://cryptic-scrubland-09574.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
